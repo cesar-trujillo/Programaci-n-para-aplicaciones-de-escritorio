f39bcadc2c2d149abd06f7e89100fe93e7a5ef3b
cesar-trujillo <zs24016729@estudiantes.uv.mx>
2026-03-25 08:47:25 -0600

    v1.3

--- markdow-editor/menu.js
+++ markdow-editor/menu.js
@@ -11,34 +11,9 @@ const template = [
                 }
             }
         ]
-    },
-    {
-        label: 'Debugging',
-        submenu: [
-            {
-                label: 'DevTools',
-                role: 'toggleDevTools'
-            },
-            { type: 'separator'},
-            { 
-                role: 'reload',
-                accelerator: 'Alt+R'
-
-            }
-        ]
     }
+    
 ];
-
-if (process.platform === 'darwin') {
-    template.unshift({
-        label: app.getName(),
-        submenu: [
-            { role: 'about' },
-            { type: 'separator' },
-            { role: 'quit' }
-        ]
-    })
-}
 
 const menu = Menu.buildFromTemplate(template);
 module.exports = menu;
